bc03df445d222a894c9aac3f9e962b3119ab0e5f
Michael Zekoff <zekoff@gmail.com>
2015-01-22 03:52:55 +0000

    Experiment with Phaser State objects.

--- breakout.js
+++ breakout.js
@@ -1,20 +1,22 @@
 var Phaser = Phaser || {};
 
 var game = new Phaser.Game();
-game.state.add('loading',{preload:preload},true);
+game.state.add('loading',{preload:preload,create:create},true);
 
 function preload() {
     console.log('preload');
     // load assets, etc.
+    game.load.image('paddle','images/paddle.png');
     game.state.add('main',{
         create: create,
         update: update
-    },true);
+    });
 }
 
 function create() {
     console.log('create');
     game.add.text(400,300,'worked',{font:'20px Arial', fill:'#FFFFFF',align:'center'}).anchor.set(0.5);
+    game.add.sprite(400,500,'paddle').anchor.set(0.5);
 }
 
 function update() {
