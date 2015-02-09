de0ca8f858b5b3f0692a55f9a0b7835bbc432903
Michael Zekoff <zekoff@gmail.com>
2015-02-09 03:28:05 +0000

    Add ball and paddle/world collision.

--- js/state/main.js
+++ js/state/main.js
@@ -1,14 +1,21 @@
-define(['phaser'], function(Phaser) {
+define(['phaser', 'objects/ball'], function(Phaser, Ball) {
     var state = new Phaser.State();
     var paddle;
+    var balls = [];
     state.create = function() {
         paddle = state.add.sprite(400, 500, 'atlas', 'paddle');
         paddle.anchor.set(0.5);
         state.physics.enable(paddle, Phaser.Physics.ARCADE);
+        state.physics.arcade.setBoundsToWorld();
+        paddle.body.immovable = true;
+        balls.push(new Ball(state, 400, 450));
+        balls[0].body.velocity.set(200,500);
+        paddle.inputEnabled = true;
+        paddle.input.enableDrag();
+        paddle.input.allowVerticalDrag = false;
     };
     state.update = function() {
-        // Phaser doesn't allow maxvelocity of 0, see physics/arcade/World.js ln 292
-        state.physics.arcade.accelerateToPointer(paddle, state.input.activePointer, 1000, 300, 1);
+        state.physics.arcade.collide(balls,paddle);
     };
     return state;
 });
